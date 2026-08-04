8953c57d2d085501ab0566456b7cb021fdb79cb0
Aloisio Barbosa <rodhat7@gmail.com>
2026-08-04 00:42:57 -0300

    fix terraform.yml

--- backend.tf
+++ backend.tf
@@ -1,5 +1,9 @@
 terraform {
   backend "s3" {
-
+    bucket         = "orange-ks8-logs"
+    key            = "vpc/dev/state.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
   }
 }
