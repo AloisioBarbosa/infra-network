2b2982ca8bdf7b13440a1c073f6414938aeb2833
Aloisio Barbosa <rodhat7@gmail.com>
2026-08-06 00:51:18 -0300

    fmt

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "orange-ks8-logs"
-    key            = "vpc/dev/state.tfstate"
-    region         = "us-east-1"
-    use_lockfile   = true
-    encrypt        = true
+    bucket       = "orange-ks8-logs"
+    key          = "vpc/dev/state.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
